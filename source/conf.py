4873007d0cec8505339a940a45c82e5b7bf81ccc
kaoutarel3 <kaoutarelrhazi2003@gmail.com>
2024-12-30 11:50:27 +0100

    change 2

--- source/conf.py
+++ source/conf.py
@@ -27,6 +27,8 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_theme_options = {
     'prev_next_buttons_location': 'bottom',  # Options: 'bottom', 'top', 'both', None
-    'style_nav_header_background': '#829ded',  # Optional: Customize navigation header color
+    'style_nav_header_background': '#070637',  # Optional: Customize navigation header color
+    'collapse_navigation': True,  # Ensures the menu doesn't collapse
+    'navigation_depth': 1,
 }
-
+html_title = "Stu(dying)"
